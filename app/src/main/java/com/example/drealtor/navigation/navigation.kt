5f33974c68c880db9a7dd4de85f890e7762addca
PeterLojiKim <pkironji@yahoo.com>
2024-01-16 08:45:04 +0300

    added navigation of the screens

--- app/src/main/java/com/example/drealtor/navigation/navigation.kt
+++ app/src/main/java/com/example/drealtor/navigation/navigation.kt
@@ -1,7 +1,6 @@
 package com.example.drealtor.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -9,6 +8,7 @@ import com.example.drealtor.screens.DetailsScreen
 import com.example.drealtor.screens.Home
 import com.example.drealtor.utils.Screens
 
+/// navgraph
 @Composable
 fun NavigationGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screens.Home.route) {
